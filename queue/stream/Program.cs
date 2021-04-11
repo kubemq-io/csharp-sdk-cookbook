c21fb6d2f2ab2f39cfa9194b31cce11dda20046d
kubemq <lior.nabat@kubemq.io>
2021-04-11 15:36:04 +0300

    Update with update sdk

--- queue/stream/Program.cs
+++ queue/stream/Program.cs
@@ -4,7 +4,7 @@ using System.Threading;
 
 namespace stream
 {
-    class Program
+       class Program
     {
         static void Main(string[] args)
         {
@@ -36,31 +36,59 @@ namespace stream
             {
                 Console.WriteLine(ex.Message);
                 System.Environment.Exit(1);
-            }
-
+           }
+            
             var receiver = new KubeMQ.SDK.csharp.Queue.Queue(QueueName, "Csharp-sdk-cookbook-queues-stream-client", KubeMQServerAddress);
-            var transaction = receiver.CreateTransaction();
-            KubeMQ.SDK.csharp.Queue.Stream.TransactionMessagesResponse resRec;
-            try
-            {
-                resRec = transaction.Receive(1, 1);
-                if (resRec.IsError)
-                {
-                    Console.WriteLine($"Message dequeue error, error:{resRec.Error}");
-                    return;
-                }
-                Console.WriteLine($"MessageID: {resRec.Message.MessageID}, Body:{KubeMQ.SDK.csharp.Tools.Converter.FromByteArray(resRec.Message.Body)}");
 
-            }
-            catch (System.Exception ex)
+            while (true)
             {
-                Console.WriteLine($"Message dequeue error, error:{ex.Message}");
-                return;
+                Console.WriteLine("Checking Messages");
+                var transaction = receiver.CreateTransaction();
+                KubeMQ.SDK.csharp.Queue.Stream.TransactionMessagesResponse resRec;
+                try
+                {
+                    resRec = transaction.Receive(1, 5);
+                    if (resRec.IsError)
+                    {
+                        Console.WriteLine($"Message dequeue error, error:{resRec.Error}");
+                        transaction.Close();
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            $"MessageID: {resRec.Message.MessageID} Received");
+                        try
+                        {
+                            var ackRes = transaction.AckMessage(resRec.Message.Attributes.Sequence);
+                            if (ackRes.IsError)
+                            {
+                                Console.WriteLine($"Error in ack Message, error:{ackRes.Error}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Ack completed");
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            throw;
+                        }
+                        finally
+                        {
+                            transaction.Close();
+                        }
+                    }
+
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine($"Message Receive error, error:{ex.Message}");
+                }
             }
-            Console.WriteLine("Doing some work.....");
-            Thread.Sleep(1000);
-            Console.WriteLine("DONE");
         }
+           
     }
    
 }
